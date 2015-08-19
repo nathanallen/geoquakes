65d8f3e1ca96b0d8d8c4290849e7ce894a15f64b
nathanallen <thenathanator@gmail.com>
2015-08-19 16:37:37 -0700

    slight refactor

--- base.js
+++ base.js
@@ -1,23 +1,24 @@
+// globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
-
+var $info_row_target;
 var map;
 
 $(document).ready(function(){
 
-  // CREATE MAP
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 37.78, lng: -122.44},
-    zoom: 8
-  });
+  $info_row_target = $("#info");
 
-  // FETCH QUAKE DATA
+  createMap();
+  fetchQuakeData();
+
+});
+
+function fetchQuakeData(){
   $.get(weekly_quakes_endpoint, function(response){
-    response.features.forEach(function(quake){
-
-      // AND INFO ROW
+    response.features.forEach(function renderRowAndMarker(quake){
+      // ADD INFO ROW
       var title = quake.properties.title;
       var hours_ago = Math.round( ( Date.now() - quake.properties.time ) / (1000*60*60) );
-      $("#info").append( "<p>" + title + " / " + hours_ago + " hours ago</p>");
+      $info_row_target.append( "<p>" + title + " / " + hours_ago + " hours ago</p>");
 
       // CREATE MARKER
       var lat = quake.geometry.coordinates[1];
@@ -27,9 +28,15 @@ $(document).ready(function(){
         map: map,
         title: title
       });
-
-    })
+    });
   })
+}
 
-});
+function createMap(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 37.78, lng: -122.44},
+    zoom: 1
+  });
+}
+
 
