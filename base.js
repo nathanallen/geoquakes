f07d817c8d48cc7692117c5a647ba910765d2753
nathanallen <thenathanator@gmail.com>
2015-08-19 15:08:22 -0700

    starter_code

--- base.js
+++ base.js
@@ -1,1 +1,13 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+
+var map;
+
+$(document).ready(function(){
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+
+})
+
